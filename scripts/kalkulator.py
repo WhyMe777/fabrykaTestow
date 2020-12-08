0979df339342d004047ef31f15c5be73a141c6bf
WhyMe777 <aborcz777@gmail.com>
2020-12-08 17:03:31 +0100

    8.12.2020 - kalkulator

--- scripts/kalkulator.py
+++ scripts/kalkulator.py
@@ -11,22 +11,31 @@ def divide(x, y):
     return x / y
 
 
-print("Wybierz operację.")
-print("1.Dodawanie")
-print("2.Odejmowanie")
-print("3.Mnożenie")
-print("4.Dzielenie")
+print("Wybierz operację:")
+print("(1) Dodawanie")
+print("(2) Odejmowanie")
+print("(3) Mnożenie")
+print("(4) Dzielenie")
 
 while True:
-    choice = input("Wybierz operację(1/2/3/4): ")
+    choice = input("Wybierz operację (1/2/3/4): ")
 
     if choice in ('1', '2', '3', '4'):
         num1 = float(input("Podaj pierwszą liczbę: "))
         num2 = float(input("Podaj drugą liczbę: "))
 
 
-# tu wpisz swój kod :)
-
+# kod
+        if choice == '1':
+            print(add(num1, num2))
+        elif choice == '2':
+            print(subtract(num1, num2))
+        elif choice == '3':
+            print(multiply(num1, num2))
+        elif choice == '4':
+            print(divide(num1, num2))
+        break
+# koniec
 
     else:
-        print("Błędna wartość, podaj poprawną")
+        print("Podano niepoprawną wartość")
